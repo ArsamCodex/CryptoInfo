787126738e8ff56e1b1d26e9d42ffef584598005
Arii <arsamparsa777@gmail.com>
2021-03-26 08:48:46 +0100

    I

--- src/main/java/AntiVirusList/MainController.java
+++ src/main/java/AntiVirusList/MainController.java
@@ -1,6 +1,18 @@
 package AntiVirusList;
 
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,33 +21,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.*;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 
 public class MainController {
+    static String apiKey = "b4666977-739e-47bd-8ad6-02ad69bfd85f";
 
 
     @GetMapping("/")
     public String Home(@RequestParam(value = "tall1", required = false) String tall1,
 
 
-                       @RequestParam(value = "tall4", required = false) String tall4,
-                       @RequestParam(value = "mathType", required = false) String mathType, Model model) throws IOException, NoSuchAlgorithmException {
+                       @RequestParam(value = "mathType", required = false) String mathType, Model model) throws Exception {
 
-        if (tall1 != null   && mathType != null) {
-            switch (mathType) {
-                case "pluss":
-
-                    int ta = Integer.parseInt(tall4);
+        if (tall1 != null && mathType != null) {
+            switch (tall1) {
+                case "binance":
 
 
-                    String find = convertStringToBinary(tall1);
-                    model.addAttribute("svar", find);
+                    model.addAttribute("svar", getUrlContents(tall1));
+
 
 //                    String shutdownCmd = "shutdown -s";
 
                     break;
+                case "coinmarketcap":
+                    String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+                    List<NameValuePair> paratmers = new ArrayList<NameValuePair>();
+                    paratmers.add(new BasicNameValuePair("start", "1"));
+                    paratmers.add(new BasicNameValuePair("limit", "5"));
+                    paratmers.add(new BasicNameValuePair("convert", "USD"));
+
+                    makeAPICall(uri,paratmers);
+                    System.out.println(makeAPICall(uri,paratmers));
+
                 default:
                     break;
             }
@@ -46,20 +74,75 @@ public class MainController {
     }
 
 
-    public static String convertStringToBinary(String input) {
+//    public void getDataFromApi(String exchange) throws IOException {
+//
+//        URL url = new URL("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
+//        URLConnection con = url.openConnection();
+//
+//        con.setDoOutput(true); // we want the response
+//        con.setRequestProperty("Cookie", "myCookie=test123");
+//        con.connect();
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//
+//        StringBuilder response = new StringBuilder();
+//        String inputLine;
+//
+//        String newLine = System.getProperty("line.separator");
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine + newLine);
+//        }
+//
+//        in.close();
 
-        StringBuilder result = new StringBuilder();
-        char[] chars = input.toCharArray();
-        for (char aChar : chars) {
-            result.append(
-                    String.format("%8s", Integer.toBinaryString(aChar))   // char -> int, auto-cast
-                            .replaceAll(" ", "0")                         // zero pads
-            );
+//    }
+
+    private static String getUrlContents(String theUrl) throws IOException {
+        URL uri = new URL("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
+        URLConnection ec = uri.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                ec.getInputStream(), "UTF-8"));
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            a.append(inputLine);
+        in.close();
+
+        System.out.println(a.toString());
+        return a.toString();
+
+    }
+
+    public static String makeAPICall(String uri, List<NameValuePair> parameters)
+            throws URISyntaxException, IOException {
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
         }
-        return result.toString();
 
+        return response_content;
     }
 
 
 }
+
+
+
 
